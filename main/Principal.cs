dd3235768e669f591ba302e2f9a06f65efaffb7a
João Victor Sierra <jvpsierra@gmail.com>
2020-08-26 13:37:27 -0300

    Quinto commit

--- main/Principal.cs
+++ main/Principal.cs
@@ -5,6 +5,7 @@ class Principal
     static void Main(string[] args)
     {
         Console.WriteLine("Commit 3 - João");
-        Console.WriteLine("Commit 4 - João")
+        Console.WriteLine("Commit 4 - João");
+        Console.WriteLine("Commit 5 - João");
     }
 }
